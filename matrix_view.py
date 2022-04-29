5d9877e1eb22b631b90d37bd055ba3b3551873c3
Diego Esparza <dae1@williams.edu>
2022-04-29 00:03:24 -0400

    simulate numa machine

--- matrix_view.py
+++ matrix_view.py
@@ -6,6 +6,8 @@ f_name = sys.argv[1]
 
 f = np.genfromtxt(f_name, delimiter=",")
 
-plt.imshow(f, cmap='gray')
+without_headers = np.delete(np.delete(f, 0, 0), 0, 1)
+
+plt.imshow(without_headers, cmap='binary')
 plt.show()
 
---
+++ simulation/numa.ml
@@ -0,0 +1,68 @@
+type machine = int array array;;
+type mapping = int array;;
+type thread_page_mapping = {thread_mapping: mapping;
+                            page_mapping: mapping};;
+
+type access_info = {thread: int; page: int};;
+type access_type = Store | Load;;
+type access = access_type * access_info;;
+
+type remapping_info = {agent: agent; destination: processor};;
+type remapping_type = Thread_remap | Page_remap;;
+type remapping = remapping_type * remapping_info;;
+
+type simulation_step = Access of access | Remapping of remapping;;
+type trace = simulation_step list;;
+
+
+let trace_file = "test.txt";;
+let read_trace file =
+  let ic = open_in file in
+  let line = input_line ic in
+  ...continue from here, with regex
+
+(* Running simulation *)
+let distance machine processor1 processor2 =
+  Array.get (Array.get machine processor1) processor2;;
+
+let processor mapping agent =
+  try
+    Array.get mapping agent
+  with Invalid_argument ->
+        let () = Printf.printf
+                   "Attempting to access element %d in mapping of length %d\n"
+                   agent
+                   (Array.length mapping) in
+        raise Invalid_argument;;
+
+let remap mapping {agent; destination} =
+  let () = Array.set mapping agent destination in
+  mapping;;
+
+let cost machine {thread_mapping; page_mapping} (_, {thread; page}) =
+  let p1 = processor thread_mapping thread in
+  let p2 = processor page_mapping page in
+  distance machine p1 p2;;
+
+
+let rec simulate machine {thread_mapping; page_mapping} = function
+  | [] -> 0
+  | Remapping (Thread_remap, r)::rest ->
+     simulate
+       machine
+       {thread: (remap thread_mapping r);
+        page: page}
+       rest
+  | Remapping (Page_remap, r)::rest ->
+     simulate
+       machine
+       {thread: thread;
+        page: (remap page_mapping r}
+       rest
+  | Access (a)::rest -> (cost machine {thread_mapping; page_mapping} a) +
+                          (simulate
+                             machine
+                             {thread_mapping; page_mapping}
+                             rest);;
+
+
